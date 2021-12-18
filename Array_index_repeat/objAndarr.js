8568ea7fec01ef64c91f1ee18f9d7a78dad7bf14
hamedrafiei <hamed.rafie78@gmail.com>
2021-12-18 12:40:40 +0330

    error fixed

--- Array_index_repeat/objAndarr.js
+++ Array_index_repeat/objAndarr.js
@@ -1,18 +1,18 @@
-// function myFunc (myArr){
-//     let theObj = {
-//         min : myArr[max&min_object],
-//         max : myArr[max&min_object],
-//     }
-//     for (let i = Array_index_repeat ; i < myArr.length ; i++){
-//         if (myArr[i] < theObj.min) {
-//             theObj.min = myArr[i]
-//         }
-//         if (myArr[i] > theObj.max){
-//             theObj.max = myArr[i]
-//         }
-//     }
-//     return theObj;
-// }
-// let myArr = [5,4,6,8,9,8,7,2,3]
-// console.log(myFunc(myArr))
-//--------------------------------------------------------
+function myFunc (myArr){
+    let theObj = {
+        min : myArr[0],
+        max : myArr[0],
+    }
+    for (let i = 0 ; i < myArr.length ; i++){
+        if (myArr[i] < theObj.min) {
+            theObj.min = myArr[i]
+        }
+        if (myArr[i] > theObj.max){
+            theObj.max = myArr[i]
+        }
+    }
+    return theObj;
+}
+let myArr = [5,4,6,8,9,8,7,2,3]
+console.log(myFunc(myArr))
+
